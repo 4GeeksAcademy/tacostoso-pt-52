73b5949bc54d1f5bdf7dcbdd7c5e32a1887ffd11
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2025-08-05 02:25:15 +0000

    :rocket: Add database models and migrations for Taco, Protein, Sauce, and associations

--- src/api/models.py
+++ src/api/models.py
@@ -1,6 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, Enum, ForeignKey, Float, Integer
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from typing import List
+from enum import Enum as pyenum
 
 db = SQLAlchemy()
 
@@ -10,10 +13,63 @@ class User(db.Model):
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
-
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+class Tortilla(pyenum):
+    HARINA = 1
+    MAIZ = 2
+
+
+class Protein(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(16), nullable=False)
+    price: Mapped[float] = mapped_column(Float(2), nullable=False)
+    # tacos
+
+
+class Spice(pyenum):
+    BAJO = 0
+    POCO = 1
+    MEDIO = 2
+    ALTO = 3
+    AGRESIVO = 4
+    ARRANCA_GARGANTAS = 5
+
+
+assosiation_sauces = db.Table('assosiation_sauces', db.metadata,
+                              db.Column('taco_id', Integer,
+                                        ForeignKey('taco.id')),
+                              db.Column('sauce_id', Integer,
+                                        ForeignKey('sauce.id'))
+                              )
+
+
+class Sauce(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(16), nullable=False)
+    spice: Mapped[Enum] = mapped_column(Enum(Spice), nullable=False)
+
+    tacos: Mapped[List["Taco"]] = relationship(
+        secondary=assosiation_sauces, back_populates="sauces"
+    )
+
+
+class Taco(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    tortilla: Mapped[Enum] = mapped_column(Enum(Tortilla), nullable=False)
+
+    protein_id: Mapped[int] = mapped_column(
+        ForeignKey("protein.id"), nullable=False
+    )
+    protein: Mapped["Protein"] = relationship(backref="tacos")
+
+    sauces: Mapped[List["Sauce"]] = relationship(
+        secondary=assosiation_sauces, back_populates="tacos"
+    )
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,9 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+
+from api.models import db, User, Protein, Sauce, Taco
+
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
